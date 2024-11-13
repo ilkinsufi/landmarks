ac49f352edaea081e0f71aba367d994c7faa514b
ilkinsufi <ilkinsufi@gmail.com>
2024-11-13 12:22:57 +0400

    Project done!

---
+++ LandMarks/LandMarks/Views/LandmarkRow.swift
@@ -0,0 +1,18 @@
+//
+//  LandmarkRow.swift
+//  LandMarks
+//
+//  Created by Ilkin Mammadov on 13.11.24.
+//
+
+import SwiftUI
+
+struct LandmarkRow: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LandmarkRow()
+}
