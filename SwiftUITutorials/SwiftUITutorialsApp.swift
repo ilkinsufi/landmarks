5be9901146b1d160a2d93a9707c543108ccfe27d
ilkinsufi <ilkinsufi@gmail.com>
2024-11-13 09:12:09 +0400

    Initial Commit

---
+++ SwiftUITutorials/SwiftUITutorialsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUITutorialsApp.swift
+//  SwiftUITutorials
+//
+//  Created by Ilkin Mammadov on 13.11.24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUITutorialsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SwiftUITutorialsTests/SwiftUITutorialsTests.swift
@@ -0,0 +1,16 @@
+//
+//  SwiftUITutorialsTests.swift
+//  SwiftUITutorialsTests
+//
+//  Created by Ilkin Mammadov on 13.11.24.
+//
+
+import Testing
+
+struct SwiftUITutorialsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
